24d823f85ce2819ca57715ee28760fedaf1bef40
rogeriogr <rogerio.ead@rogerio.rodrigues.nom.br>
2025-12-29 04:10:59 -0300

    fix: corrigindo nome do pacote supabase no import

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-client';
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
